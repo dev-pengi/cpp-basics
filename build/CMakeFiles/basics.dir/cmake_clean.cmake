507a1b0e71281cc38454fead02595760b33e736f
dev-pengi <siffffdoul@gmail.com>
2024-09-13 01:29:22 +0100

    first commit

---
+++ build/CMakeFiles/basics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basics.dir/C_/Users/Sif/Desktop/cppbasics/main.cpp.o"
+  "c:/Users/Sif/Desktop/cppbasics/build/basics.exe"
+  "c:/Users/Sif/Desktop/cppbasics/build/basics.pdb"
+  "c:/Users/Sif/Desktop/cppbasics/build/libbasics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/basics.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for basics.
+# This may be replaced when dependencies are built.
